023076431cca10ba11154bd08a26c12acf624844
mzaien <s201540830@kfupm.edu.sa>
2021-01-20 09:37:16 +0300

    Adding the designed Header and some updates🤣🏃‍♂️

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,22 @@
+import { Flex, IconButton } from "@chakra-ui/react"
+import { SearchIcon } from "@chakra-ui/icons"
 import { Link } from "gatsby"
+// import Img from "gatsby-image"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => <header>{/* Som */}</header>
+const Header = ({ siteTitle }) => (
+  <header>
+    {" "}
+    <Flex direction="row" justify="space-evenly" py={4} align="center">
+      <img alt="Logo" src="/public/icons/icon-96x96.png" />
+      <Link>عن النادي</Link>
+      <Link>التسجيل</Link>
+      <Link>تواصل معنا</Link>
+      <IconButton aria-label="Search database" icon={<SearchIcon />} />
+    </Flex>
+  </header>
+)
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
