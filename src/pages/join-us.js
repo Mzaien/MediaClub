beef981436b4775f391848ab47b40cdbfc077cc2
Abdullah Mzaien <s201540830@kfupm.edu.sa>
2021-02-15 14:02:05 +0300

    Deleted packages from the code 🦹‍♂️

--- src/pages/join-us.js
+++ src/pages/join-us.js
@@ -1,4 +1,3 @@
-import { useForm } from "react-hook-form"
 import React from "react"
 import {
   FormErrorMessage,
@@ -11,56 +10,22 @@ import {
 } from "@chakra-ui/react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import axios from "axios"
 const JoinUsPage = () => {
-  const { register, handleSubmit, errors } = useForm()
-  const cors = "https://shrouded-earth-67675.herokuapp.com/"
-  const link = `https://docs.google.com/forms/d/e/1FAIpQLSe0jE7c36Me40FX1Ir-NXAFhwA5VSIz2bqYPV0Bx7f47iv-EQ/formResponse`
-  const onSubmit = data => {
-    const formData = new FormData(data.target)
-    axios({
-      url: `${cors}${link}`,
-      method: "post",
-      data: formData,
-      responseType: "json",
-    })
-      .then(res => {
-        console.log("response", res)
-      })
-      .catch(err => {
-        console.log("err", err)
-      })
-    console.log(data)
-  }
   return (
     <Layout>
       <SEO title="الانضمام للنادي" />
       <Heading as="h1">انضم للنادي</Heading>
       <Box p={8}>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form method="POST" data-netlify="true" netlify-honeypot="bot-field">
           <FormControl isRequired>
             <FormLabel htmlFor="name">اسمك الكريم</FormLabel>
-            <Input
-              type="text"
-              placeholder="Name"
-              name="Name"
-              ref={register({ required: true })}
-            />
-            <FormErrorMessage>
-              {errors.Name && errors.Name.message}
-            </FormErrorMessage>
+            <Input type="text" placeholder="Name" name="Name" />
+            <FormErrorMessage></FormErrorMessage>
           </FormControl>
           <FormControl isRequired>
             <FormLabel htmlFor="Email">إيميلك الجامعي</FormLabel>
-            <Input
-              type="text"
-              placeholder="Email"
-              name="Email"
-              ref={register({ required: true, pattern: /^\S+@\S+$/i })}
-            />
-            <FormErrorMessage>
-              {errors.Email && errors.Email.message}
-            </FormErrorMessage>
+            <Input type="text" placeholder="Email" name="Email" />
+            <FormErrorMessage></FormErrorMessage>
           </FormControl>
           <FormControl isRequired>
             <FormLabel htmlFor="Mobile number">رقم جوالك</FormLabel>
@@ -68,11 +33,8 @@ const JoinUsPage = () => {
               type="tel"
               placeholder="Mobile number"
               name="Mobile number"
-              ref={register({ required: true, minLength: 6, maxLength: 13 })}
             />
-            <FormErrorMessage>
-              {errors.Mobile_number && errors.Mobile_number.message}
-            </FormErrorMessage>
+            <FormErrorMessage></FormErrorMessage>
           </FormControl>
           <Button mt={4} colorScheme="linkedin" type="submit">
             أرسل
