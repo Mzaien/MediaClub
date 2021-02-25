b360a82e3a8710c411a1b0705b00d961dd902aea
Abdullah Mzaien <s201540830@kfupm.edu.sa>
2021-02-26 00:22:45 +0300

    Added the redirect to شكرا page

--- src/pages/join-us.js
+++ src/pages/join-us.js
@@ -7,32 +7,21 @@ import {
   Box,
   Heading,
 } from "@chakra-ui/react"
-import { navigate } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-// function encode(data) {
-//   return Object.keys(data)
-//     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-//     .join("&")
-// }
 const JoinUsPage = () => {
-  // const [state, setState] = React.useState({})
-  // const handleChange = e => {
-  //   setState({ ...state, [e.target.name]: e.target.value })
-  // }
   const FORMSPARK_ACTION_URL = "https://submit-form.com/yiVEQZ6W"
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    navigate("شكرا")
-  }
   return (
     <Layout>
       <SEO title="الانضمام للنادي" />
       <Heading as="h1">انضم للنادي</Heading>
       <Box p={8}>
         <form method="POST" action={FORMSPARK_ACTION_URL}>
-          <input type="hidden" name="form-name" value="contact" />
+          <input
+            type="hidden"
+            name="_redirect"
+            value="https://media-club.vercel.app/%D8%B4%D9%83%D8%B1%D8%A7/"
+          />
           <FormControl isRequired>
             <FormLabel htmlFor="name">اسمك الكريم</FormLabel>
             <Input type="text" placeholder="عبدالله السلطان" name="Name" />
@@ -53,13 +42,7 @@ const JoinUsPage = () => {
               name="Mobile number"
             />
           </FormControl>
-          <Button
-            aria-label="أرسل"
-            mt={4}
-            colorScheme="linkedin"
-            type="submit"
-            onSubmit={handleSubmit}
-          >
+          <Button aria-label="أرسل" mt={4} colorScheme="linkedin" type="submit">
             أرسل
           </Button>
         </form>
