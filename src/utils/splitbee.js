28389c43094cc43fdb40c53a1278dc38e03c0f4e
Abdullah Mzaien <s201540830@kfupm.edu.sa>
2021-08-31 13:53:52 +0300

    Adding Splitbee this time Gatsby plugins was broken

---
+++ src/utils/splitbee.js
@@ -0,0 +1,15 @@
+import splitbee from "@splitbee/web"
+
+// This initiliazes Splitbee.js
+splitbee.init({
+    // To use Splitbee on another subdomain.
+    // Token can be found in project settings
+    token: process.env.Splitbee_Token, 
+    
+    // Enable cookie-less mode. Defaults to `false`
+    disableCookie: false,
+    
+    // Set custom urls when using proxying
+    scriptUrl: "https://cdn.splitbee.io/sb.js",
+    apiUrl: "https://hive.splitbee.io",
+  })
